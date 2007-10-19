69d347599394e41484c4c3bd4e59f2675154bf53
Richard Crowley <r@rcrowley.org>
2007-10-19 20:53:15 +0000

    Moved code timing and other debug stuff into gm_test
    git-svn-id: file:///root/var/svn/trunk/uploadr@43 2ccee060-bf41-0410-b5a3-d28fdbd3ea41

--- MacUploadr.app/Contents/Resources/components/gm_impl.cpp
+++ MacUploadr.app/Contents/Resources/components/gm_impl.cpp
@@ -24,67 +24,6 @@
 
 using namespace std;
 
-// Fake gettimeofday on Windows
-#ifdef XP_WIN
-#include <windows.h>
-#if defined(_MSC_VER) || defined(_MSC_EXTENSIONS)
-#define DELTA_EPOCH_IN_MICROSECS  11644473600000000Ui64
-#else
-#define DELTA_EPOCH_IN_MICROSECS  11644473600000000ULL
-#endif
-struct timezone {
-	int  tz_minuteswest;
-	int  tz_dsttime;
-};
-int gettimeofday(struct timeval *tv, struct timezone *tz) {
-	FILETIME ft;
-	unsigned __int64 tmpres = 0;
-	static int tzflag;
-	if (NULL != tv) {
-		GetSystemTimeAsFileTime(&ft);
-		tmpres |= ft.dwHighDateTime;
-		tmpres <<= 32;
-		tmpres |= ft.dwLowDateTime;
-		tmpres -= DELTA_EPOCH_IN_MICROSECS;
-		tmpres /= 10;
-		tv->tv_sec = (long)(tmpres / 1000000UL);
-		tv->tv_usec = (long)(tmpres % 1000000UL);
-	}
-	if (NULL != tz) {
-		if (!tzflag) {
-			_tzset();
-			tzflag++;
-		}
-		tz->tz_minuteswest = _timezone / 60;
-		tz->tz_dsttime = _daylight;
-	}
-	return 0;
-}
-#else
-#include <sys/time.h>
-#endif
-
-// Cheesy code timing
-struct timeval first;
-struct timeval last;
-void start_timer() {
-	gettimeofday(&first, 0);
-}
-void stop_timer(nsAString & _retval) {
-	gettimeofday(&last, 0);
-	double runtime = ((double)last.tv_sec + (double)last.tv_usec / 1000000) -
-		((double)first.tv_sec + (double)first.tv_usec / 1000000);
-	ostringstream oss;
-	oss << runtime << ";";
-	string s = oss.str();
-	char * c = (char *)s.c_str();
-	int i = 0;
-	while (*c) {
-		_retval.Insert(*c, i++);
-		++c;
-	}
-}
-
 // Convert an nsAString to a std::string
 string * conv_str(const nsAString & nsa) {
 	char * s = 0;
@@ -263,11 +202,8 @@ NS_IMETHODIMP CGM::Thumb(PRInt32 square, const nsAString & path, nsAString & _re
 		}
 
 		// Orient the image properly and return the orientation
-//start_timer();
 		Magick::Image img(*path_str);
-//stop_timer(_retval);
 		ostringstream out;
-// out << "###"; // FOR TIMERS ONLY
 		int orient = base_orient(img);
 		out << orient << "###";
 
