088b7f86e74d49dec9c598465d14ce7b5d4e117a
Cal Henderson <cal@iamcal.com>
2007-11-30 18:25:42 +0000

    added the "check for updates" menu item and the logic behind it
    git-svn-id: file:///root/var/svn/trunk/uploadr@139 2ccee060-bf41-0410-b5a3-d28fdbd3ea41

--- MacUploadr.app/Contents/Resources/chrome/content/uploadr/uploadr.js
+++ MacUploadr.app/Contents/Resources/chrome/content/uploadr/uploadr.js
@@ -55,3 +55,63 @@ var uploadr = {
 
 	}
 };
+
+function checkForUpdates(){
+
+	var um = Components.classes["@mozilla.org/updates/update-manager;1"].getService(Components.interfaces.nsIUpdateManager);
+	var prompter = Components.classes["@mozilla.org/updates/update-prompt;1"].createInstance(Components.interfaces.nsIUpdatePrompt);
+
+	// If there's an update ready to be applied, show the "Update Downloaded"
+	// UI instead and let the user know they have to restart the browser for
+	// the changes to be applied. 
+
+	if (um.activeUpdate && um.activeUpdate.state == "pending"){
+		prompter.showUpdateDownloaded(um.activeUpdate);
+	}else{
+		prompter.checkForUpdates();
+	}
+}
+
+function buildHelpMenu(){
+
+	var updates = Components.classes["@mozilla.org/updates/update-service;1"].getService(Components.interfaces.nsIApplicationUpdateService);
+	var um = Components.classes["@mozilla.org/updates/update-manager;1"].getService(Components.interfaces.nsIUpdateManager);
+
+	var checkForUpdates	= document.getElementById("menu_updates");
+	var strings		= document.getElementById("locale");
+	var activeUpdate	= um.activeUpdate;
+
+	// If there's an active update, substitute its name into the label
+	// we show for this item, otherwise display a generic label.
+	function getStringWithUpdateName(key) {
+		if (activeUpdate && activeUpdate.name) return strings.getFormattedString(key, [activeUpdate.name]);
+		return strings.getString(key + "Fallback");
+	}
+
+	// By default, show "Check for Updates..."
+	var key = "default";
+	if (activeUpdate) {
+		switch (activeUpdate.state) {
+			case "downloading":
+				// If we're downloading an update at present, show the text:
+				// "Downloading Firefox x.x..." otherwise we're paused, and show
+				// "Resume Downloading Firefox x.x..."
+				key = updates.isDownloading ? "downloading" : "resume";
+				break;
+			case "pending":
+				// If we're waiting for the user to restart, show: "Apply Downloaded
+				// Updates Now..."
+				key = "pending";
+				break;
+		}
+	}
+
+	checkForUpdates.label = getStringWithUpdateName("updatesItem_" + key);
+
+	if (um.activeUpdate && updates.isDownloading){
+		checkForUpdates.setAttribute("loading", "true");
+	}else{
+		checkForUpdates.removeAttribute("loading");
+	}
+}
+
