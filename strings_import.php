0629719bb5c92b09b952f9bc48e69e786bb84f88
Richard Crowley <r@rcrowley.org>
2008-04-03 23:22:49 +0000

    Updates to the string import script to properly fix everything from Smarty in the DTDs
    git-svn-id: file:///root/var/svn/trunk/uploadr@328 2ccee060-bf41-0410-b5a3-d28fdbd3ea41

--- strings_import.php
+++ strings_import.php
@@ -25,20 +25,55 @@ foreach (array('de-de', 'es-us', 'fr-fr', 'it-it', 'ko-kr', 'pt-br', 'zh-hk') as
 			continue;
 		}
 
-		# Clean and save
+		# Clean PHP garbage out
 		$text = file_get_contents("$argv[1]/$l/$file");
 		if (false === $text) {
 			echo "[error] Reading $file\n";
 			continue;
 		}
 		$text = trim(str_replace("\n\n", "\n", preg_replace('/^<\?php.*\?>$/ms', '', $text)));
+
+		# Process each string
+		$lines = explode("\n", $text);
+		$ii = sizeof($lines);
+		$replace = array();
+		for ($i = 0; $i < $ii; ++$i) {
+
+			# Escape & and " properly
+			if (preg_match('/^(<!ENTITY [a-zA-Z0-9.]+ ")(.+)(">)$/', $lines[$i], $s)) {
+				$lines[$i] = $s[1] . str_replace('"', '&#34;',
+					preg_replace('/&(?!#\d\d;)/', '&#38;', $s[2])) . $s[3];
+			}
+
+			# Split up combined complicated strings
+			if (preg_match('/^<!ENTITY ([a-zA-Z0-9.]+) "(.+)">$/', $lines[$i], $s)
+				&& preg_match('/^(.+)\.joined$/', $s[1], $k)) {
+				$parts = explode('  ', $s[2]);
+				if (2 == sizeof($parts)) {
+					$replace["{$k[1]}.1"] = "{$parts[0]} ";
+					$replace["{$k[1]}.2"] = " {$parts[1]}";
+					$lines[$i] = '';
+				}
+			}
+
+		}
+
+		# Replace split up versions with their translations
+		for ($i = 0; $i < $ii; ++$i) {
+			if (preg_match('/^<!ENTITY ([a-zA-Z0-9.]+) ".+">$/', $lines[$i], $s)
+				&& isset($replace[$s[1]])) {
+				$lines[$i] = "<!ENTITY {$s[1]} \"{$replace[$s[1]]}\">";
+			}
+		}
+
+		# Write and save
 		$file = next($match);
 		$file_p = fopen("$locale/$l/$file", 'w');
 		if (false === $file_p) {
 			echo "[error] Opening $file\n";
 			continue;
 		}
-		if (false === fwrite($file_p, $text)) {
+		if (false === fwrite($file_p, array_diff($lines, array('')))) {
 			echo "[error] Writing $file\n";
 		}
 		if (false === fclose($file_p)) {
