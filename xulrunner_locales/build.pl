6f3c569ead09aaeebb3b0e5610557e9f1220778d
Cal Henderson <cal@iamcal.com>
2008-04-17 19:52:54 +0000

    added the source of the locale JARs, along with a build script to generate them (we only store the diffs for each language). the updated jars looks to be near identical (according to my diff program) to the old ones.
    git-svn-id: file:///root/var/svn/trunk/uploadr@335 2ccee060-bf41-0410-b5a3-d28fdbd3ea41

---
+++ xulrunner_locales/build.pl
@@ -0,0 +1,84 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use Cwd;
+
+$|++;
+
+
+#
+# 1. Build the locale list
+#
+
+my $dir = getcwd;
+my $locales = {};
+
+opendir DH, "$dir/locale" or die "can't open locale folder: $!";
+while (my $name = readdir(DH)){
+
+	if ($name =~ m/^[a-z]{2}-[a-z]{2}$/i){
+
+		$locales->{$name} = 1;
+	}
+}
+closedir DH;
+
+
+#
+# 2. build the JARs
+#
+
+`rm $dir/??-??.*`;
+
+mkdir "$dir/build";
+`rm -r $dir/build/*`;
+
+for my $locale(keys %{$locales}){
+
+	print "Building $locale.jar...";
+
+	mkdir "$dir/build/locale";
+	mkdir "$dir/build/locale/$locale";
+
+	`cp -r $dir/locale/en-US/* $dir/build/locale/$locale/`;
+
+	if ($locale ne 'en-US'){
+
+		`cp -r $dir/locale/$locale/* $dir/build/locale/$locale/`;
+	}
+
+	chdir "$dir/build";
+
+	`7z.exe a -r -tzip $locale.zip locale -mx1`;
+	`mv $locale.zip ../$locale.jar`;
+
+	`rm -r $dir/build/locale`;
+
+	print "ok\n";
+}
+
+`rm -r $dir/build`;
+
+
+#
+# 3. build the manifests
+#
+
+chdir $dir;
+
+for my $locale(keys %{$locales}){
+
+	print "building $locale.manifest...";
+
+	`perl -p -e 's/LOCALE/$locale/g' manifest.template > $locale.manifest`;
+
+	print "ok\n";
+}
+
+
+#
+# 4. all done!
+#
+
+print "ok, all done!\n";
