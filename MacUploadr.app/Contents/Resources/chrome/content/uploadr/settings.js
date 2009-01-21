7720a9008d11b200be337e1547eef8e7988ef71d
Jerome Decq <jerome@flickr.com>
2009-01-21 14:52:25 +0000

    update build number and year for copyright
    git-svn-id: file:///root/var/svn/trunk/uploadr@501 2ccee060-bf41-0410-b5a3-d28fdbd3ea41

--- MacUploadr.app/Contents/Resources/chrome/content/uploadr/settings.js
+++ MacUploadr.app/Contents/Resources/chrome/content/uploadr/settings.js
@@ -1,7 +1,7 @@
 /*
  * Flickr Uploadr
  *
- * Copyright (c) 2007-2008 Yahoo! Inc.  All rights reserved.  This library is
+ * Copyright (c) 2007-2009 Yahoo! Inc.  All rights reserved.  This library is
  * free software; you can redistribute it and/or modify it under the terms of
  * the GNU General Public License (GPL), version 2 only.  This library is
  * distributed WITHOUT ANY WARRANTY, whether express or implied. See the GNU
